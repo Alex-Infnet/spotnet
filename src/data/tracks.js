d883e34884807cf30aac10f4243b3408e16265f7
Alexandre Werneck <xanwerneck@gmail.com>
2023-10-30 09:03:17 -0300

    origin code

---
+++ src/data/tracks.js
@@ -0,0 +1,14 @@
+export const tracks = [
+  {
+    name: 'Song 1',
+    genre: ['Pop'],
+  },
+  {
+    name: 'Song 2',
+    genre: ['Reggae'],
+  },
+  {
+    name: 'Song 3',
+    genre: ['Pop', 'Funk'],
+  },
+];
